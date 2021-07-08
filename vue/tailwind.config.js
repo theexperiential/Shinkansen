8d1ea924b2da7e9a74a6b87a6ff2a8dbca6ae197
Dylan Roscover <rosco@theexperiential.com>
2021-07-08 13:19:34 -0700

    add tailwindcss

---
+++ vue/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './public/**/*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+      ],
+    darkMode: 'media',
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
